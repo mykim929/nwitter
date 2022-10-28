218d321775826205382f035104b63858d98f5528
mykim929 <mykim929@nexon.co.kr>
2022-10-28 13:36:01 +0900

    소셜로그인에서 이름 가져오기, 이름 수정하여 업데이트하기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,21 +1,50 @@
 import { useNavigate } from 'react-router-dom';
 import { authService, dbService } from "fbase";
-import { collection, onSnapshot } from 'firebase/firestore';
-import { useEffect } from "react";
+import { collection, query, where, orderBy, onSnapshot } from 'firebase/firestore';
+import { updateProfile } from "firebase/auth";
+import { useEffect, useState } from "react";
 
-const Profile = ({ userObj }) => {
+const Profile = ({ userObj, refreshUser }) => {
     const navigate = useNavigate();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
     const onLogOutClick = () => {
         authService.signOut();
         navigate('/');
     };
-    const getMyNweets = async() => {
-        onSnapshot(collection(dbService, 'nweets'),(snapshot) => {
-        });
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
     }
-    useEffect(() => {}, []);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await updateProfile(authService.currentUser, {
+                displayName: newDisplayName
+            });
+            refreshUser();
+        }
+    }
+    /*
+    const getMyNweets = async () => {
+        const nweets = await query(collection(dbService, 'nweets'), where("creatorId", "==", userObj.uid), orderBy("createdAt", "asc"));
+        const unsubscribe = onSnapshot(nweets, (querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                console.log(doc.data());
+            });
+        });
+    };
+    useEffect(() => {
+        getMyNweets();
+    }, []);
+    */
     return(
         <>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} type="text" placeholder="Display name" value={newDisplayName} />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </>
     )
